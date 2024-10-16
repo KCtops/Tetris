b58fd919daac1754b2b45e3a243d25361ed85cab
Kyle Collamer <126096649+KCtops@users.noreply.github.com>
2024-10-16 14:25:25 -0400

    Update Main.java
    added comments.

--- src/Main.java
+++ src/Main.java
@@ -13,17 +13,17 @@ public class Main {
      * @param args command line arguments.
      */
     public static void main(String[] args) {
-        double delay = 0;
+        double delay = 0;    // initalize delay to zero.
         do {
-            Scanner scanner = new Scanner(System.in);
-            System.out.println("What do you want the delay to be in seconds?");
-            System.out.print("ENTER DELAY : ");
-            delay = scanner.nextDouble();
-            if (delay < 0.125) {
-                System.out.println("ERROR : Delay is too fast.");
-                System.out.println();
+            Scanner scanner = new Scanner(System.in);    // initalize new scanner to get double.
+            System.out.println("What do you want the delay to be in seconds?");    // prompts the user to enter the delay.
+            System.out.print("ENTER DELAY : ");    // prompts the user to enter the delay.
+            delay = scanner.nextDouble();    // get the double the user enters.
+            if (delay < 0.125) {    // if the delay is too fast...
+                System.out.println("ERROR : Delay is too fast.");    // ...notify the user that the delay is too fast.
+                System.out.println();    // print a space.
             }
-        } while (delay < 0.125);
-        new TetrisFrame(delay);
+        } while (delay < 0.125);    // while the entered delay is invalid...
+        new TetrisFrame(delay);    // create a new Tetris game.
     }
 }
